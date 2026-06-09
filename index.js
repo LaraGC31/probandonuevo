8549b1a52fb8c4b7fa732a401cefca63e81ec973
lara <larita.gonzal.carranco@gmail.com>
2026-06-09 19:35:02 +0200

    guardado una nueva forma

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-window.onload= function() {
-mostrarJugadores();
+window.onload = function () {
+    mostrarJugadores();
+    mostrarJugadoresAtm();
+   todo();
 }
-function mostrarJugadores() {
 
-    const contenedor = document.getElementById("jugadores");
+
+document.addEventListener('DOMContentLoaded', todo);
+function mostrarJugadores() {
+    let contenedor = document.getElementById("jugadores");
+
+    if (!contenedor) return;
 
     for (let i = 1; i < 11; i++) {
         const img = document.createElement("img");
-        img.src = "fcb/imagen-"+i + ".jpg";
+        img.src = `fcb/imagen-${i}.jpg`;
         img.alt = "Jugador";
+        contenedor.appendChild(img);
+    }
+}
+function mostrarJugadoresAtm() {
+    let contenedor = document.getElementById("jugadoresatm");
+
+    if (!contenedor) return;
+
+    for (let i = 1; i < 2; i++) {
+        const img = document.createElement("img");
+        img.src = `atm/imagen-${i}.jpg`;
+        img.alt = "JugadorATM";
         contenedor.appendChild(img);
     }
 }
