8878998c5459b30a11d58a520767bba3e52e238c
LaraGC31 <larita.gonzal.carranco@gmail.com>
2026-06-13 17:16:12 +0200

    update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function mostrarJugadores() {
 
     for (let i = 1; i < 20; i++) {
         const img = document.createElement("img");
-        img.src = `fcb/imagen-${i}.jpg`;
+        img.src = `fcb/imagen-${i}.png`;
         img.alt = "Jugador";
         contenedor.appendChild(img);
     }
