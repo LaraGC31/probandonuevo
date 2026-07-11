eca96a1f175bc9e42555b9b5b407e32fb09f8efc
lara <larita.gonzal.carranco@gmail.com>
2026-07-11 14:50:42 +0200

    MMM

--- index.js
+++ index.js
@@ -23,4 +23,33 @@ fetch("b.json")
 
 });
 }
+function atm() {
+    const plantilla = document.querySelector("#plantillaJugadorATM");
+const contenedor = document.querySelector(".contenedor");
+
+fetch("atm.json")
+.then(r => r.json())
+.then(datos => {
+
+    datos.forEach(jugador => {
+
+        const copia = plantilla.content.cloneNode(true);
+
+        copia.querySelector("img").src = jugador.imagen;
+        copia.querySelector("img").alt = jugador.nombre;
+
+        copia.querySelector("h2").textContent = jugador.nombre;
+        copia.querySelector(".dorsal").textContent = "👕 " + jugador.dorsal;
+        copia.querySelector(".posicion").textContent = jugador.posicion;
+
+        contenedor.appendChild(copia);
+
+    });
+
+});
+}
+window.onload = function(){
+    fcb();
+    atm();
+}
     
