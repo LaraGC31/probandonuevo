022ce1749944c1b2e635cdb2059ecc547b670a5a
lara <larita.gonzal.carranco@gmail.com>
2026-07-11 18:22:13 +0200

    n

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function fcb() {
     const plantilla = document.querySelector("#plantillaJugador");
 const contenedor = document.querySelector(".contenedor");
@@ -48,8 +49,105 @@ fetch("atm.json")
 
 });
 }
+function sevilla() {
+    const plantilla = document.querySelector("#plantillaJugadorSev");
+const contenedor = document.querySelector(".contenedor");
+
+fetch("sevilla.json")
+.then(r => r.json())
+.then(datos => {
+
+    datos.forEach(jugador => {
+
+        const copia = plantilla.content.cloneNode(true);
+
+    
+        copia.querySelector("h2").textContent = jugador.nombre;
+        copia.querySelector(".dorsal").textContent = "👕 " + jugador.dorsal;
+        copia.querySelector(".posicion").textContent = jugador.posicion;
+
+        contenedor.appendChild(copia);
+
+    });
+
+});
+}
+function real() {
+    const plantilla = document.querySelector("#plantillaJugadorATM");
+const contenedor = document.querySelector(".contenedor");
+
+fetch("atm.json")
+.then(r => r.json())
+.then(datos => {
+
+    datos.forEach(jugador => {
+
+        const copia = plantilla.content.cloneNode(true);
+
+        copia.querySelector("img").src = jugador.imagen;
+        copia.querySelector("img").alt = jugador.nombre;
+
+        copia.querySelector("h2").textContent = jugador.nombre;
+        copia.querySelector(".dorsal").textContent = "👕 " + jugador.dorsal;
+        copia.querySelector(".posicion").textContent = jugador.posicion;
+
+        contenedor.appendChild(copia);
+
+    });
+
+});
+}
+function villa() {
+    const plantilla = document.querySelector("#plantillaJugadorATM");
+const contenedor = document.querySelector(".contenedor");
+
+fetch("atm.json")
+.then(r => r.json())
+.then(datos => {
+
+    datos.forEach(jugador => {
+
+        const copia = plantilla.content.cloneNode(true);
+
+        copia.querySelector("img").src = jugador.imagen;
+        copia.querySelector("img").alt = jugador.nombre;
+
+        copia.querySelector("h2").textContent = jugador.nombre;
+        copia.querySelector(".dorsal").textContent = "👕 " + jugador.dorsal;
+        copia.querySelector(".posicion").textContent = jugador.posicion;
+
+        contenedor.appendChild(copia);
+
+    });
+
+});
+}
+
 window.onload = function(){
-    fcb();
-    atm();
+
+    const pagina = window.location.pathname.split("/").pop();
+
+    switch (pagina) {
+        case "barcelona.html":
+            fcb();
+            break;
+
+        case "atletico.html":
+            atm();
+            break;
+
+        case "sevilla.html":
+            sevilla();
+            break;
+
+        case "real.html":
+            real();
+            break;
+
+        case "villa.html":
+            villa();
+            break;
+    }
+
 }
     
